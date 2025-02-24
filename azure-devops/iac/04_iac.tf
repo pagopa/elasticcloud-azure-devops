7bfefb28efb314b898e49ea627c66d645e55433d
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-02-24 15:29:21 +0100

    enabled deploy pipelines

--- azure-devops/iac/04_iac.tf
+++ azure-devops/iac/04_iac.tf
@@ -110,52 +110,75 @@ module "iac_code_review" {
 ### HOW TO DEFINE A PIPELINE FOR A NEW DOMAIN?     #
 ### have a look at README.md                       #
 ####################################################
-##module "iac_deploy" {
-##  source = "./.terraform/modules/__devops_v0__/azuredevops_build_definition_deploy"
-##
-##  for_each = { for d in local.deploy_domains : d.name => d }
-##  path     = each.value.pipeline_path
-##
-##  project_id                   = data.azuredevops_project.project.id
-##  repository                   = merge(local.default_repository, each.value.repository)
-##  github_service_connection_id = azuredevops_serviceendpoint_github.azure-devops-github-pr.id
-##
-##  pipeline_name_prefix = each.value.pipeline_prefix
-##
-##  variables = merge(
-##    local.base_iac_variables,
-##    contains(each.value.envs, "d") && try(each.value.kv_name, "") != "" ? {
-##      tf_dev_aks_apiserver_url         = module.dev_secrets[each.value.name].values["p4pa-d-itn-dev-aks-apiserver-url"].value,
-##      tf_dev_aks_azure_devops_sa_cacrt = module.dev_secrets[each.value.name].values["p4pa-d-itn-dev-aks-azure-devops-sa-cacrt"].value,
-##      tf_dev_aks_azure_devops_sa_token = base64decode(module.dev_secrets[each.value.name].values["p4pa-d-itn-dev-aks-azure-devops-sa-token"].value),
-##    } : {},
-##    contains(each.value.envs, "u") && try(each.value.kv_name, "") != "" ? {
-##      tf_uat_aks_apiserver_url         = module.uat_secrets[each.value.name].values["p4pa-u-itn-uat-aks-apiserver-url"].value,
-##      tf_uat_aks_azure_devops_sa_cacrt = module.uat_secrets[each.value.name].values["p4pa-u-itn-uat-aks-azure-devops-sa-cacrt"].value,
-##      tf_uat_aks_azure_devops_sa_token = base64decode(module.uat_secrets[each.value.name].values["p4pa-u-itn-uat-aks-azure-devops-sa-token"].value),
-##    } : {},
-##    contains(each.value.envs, "p") && try(each.value.kv_name, "") != "" ? {
-##      tf_prod_aks_apiserver_url         = module.prod_secrets[each.value.name].values["p4pa-p-itn-prod-aks-apiserver-url"].value,
-##      tf_prod_aks_azure_devops_sa_cacrt = module.prod_secrets[each.value.name].values["p4pa-p-itn-prod-aks-azure-devops-sa-cacrt"].value,
-##      tf_prod_aks_azure_devops_sa_token = base64decode(module.prod_secrets[each.value.name].values["p4pa-p-itn-prod-aks-azure-devops-sa-token"].value),
-##    } : {},
-##    local.base_iac_variables_deploy,
-##    try(local.domain_variables[each.value.name].iac_variables_deploy, {})
-##  )
-##
-##  variables_secret = merge(
-##    local.base_iac_variables_secret,
-##    try(local.domain_variables[each.value.name].iac_variables_secrets_deploy, {})
-##  )
-##
-##  service_connection_ids_authorization = [
-##    azuredevops_serviceendpoint_github.azure-devops-github-ro.id,
-##    module.DEV-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_id,
-##    module.UAT-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_id,
-##    module.PROD-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_id,
-##
-##    module.DEV-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_id,
-##    module.UAT-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_id,
-##    module.PROD-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_id,
-##  ]
-##}
+module "iac_deploy" {
+ source = "./.terraform/modules/__devops_v0__/azuredevops_build_definition_deploy"
+
+ for_each = { for d in local.deploy_domains : d.name => d }
+ path     = each.value.pipeline_path
+
+ project_id                   = data.azuredevops_project.project.id
+ repository                   = merge(local.default_repository, each.value.repository)
+  github_service_connection_id = azuredevops_serviceendpoint_github.azure_devops_github_pr.id
+
+ pipeline_name_prefix = each.value.pipeline_prefix
+
+ variables = merge(
+   local.base_iac_variables,
+   contains(each.value.envs, "d") && try(each.value.kv_name, "") != "" ? {
+      tf_dev_aks_apiserver_url         = module.dev_secrets[each.value.name].values["${each.value.target}-d-${each.value.regions[0]}-dev-aks-apiserver-url"].value,
+      tf_dev_aks_azure_devops_sa_cacrt = module.dev_secrets[each.value.name].values["${each.value.target}-d-${each.value.regions[0]}-dev-aks-azure-devops-sa-cacrt"].value,
+      tf_dev_aks_azure_devops_sa_token = base64decode(module.dev_secrets[each.value.name].values["${each.value.target}-d-${each.value.regions[0]}-dev-aks-azure-devops-sa-token"].value),
+      tf_dev_aks_name = "${each.value.target}-d-${each.value.regions[0]}-dev-aks"
+    } : {},
+    contains(each.value.envs, "d") && try(each.value.kv_name, "") != "" && length(each.value.regions) > 1 ? {
+      tf_second_dev_aks_apiserver_url         = module.dev_secrets[each.value.name].values["${each.value.target}-d-${each.value.regions[1]}-dev-aks-apiserver-url"].value,
+      tf_second_dev_aks_azure_devops_sa_cacrt = module.dev_secrets[each.value.name].values["${each.value.target}-d-${each.value.regions[1]}-dev-aks-azure-devops-sa-cacrt"].value,
+      tf_second_dev_aks_azure_devops_sa_token = base64decode(module.dev_secrets[each.value.name].values["${each.value.target}-d-${each.value.regions[1]}-dev-aks-azure-devops-sa-token"].value),
+      tf_second_dev_aks_name = "${each.value.target}-d-${each.value.regions[1]}-dev-aks"
+    } : {},
+
+    contains(each.value.envs, "u") && try(each.value.kv_name, "") != "" ? {
+      tf_uat_aks_apiserver_url         = module.dev_secrets[each.value.name].values["${each.value.target}-u-${each.value.regions[0]}-uat-aks-apiserver-url"].value,
+      tf_uat_aks_azure_devops_sa_cacrt = module.dev_secrets[each.value.name].values["${each.value.target}-u-${each.value.regions[0]}-uat-aks-azure-devops-sa-cacrt"].value,
+      tf_uat_aks_azure_devops_sa_token = base64decode(module.dev_secrets[each.value.name].values["${each.value.target}-u-${each.value.regions[0]}-uat-aks-azure-devops-sa-token"].value),
+      tf_uat_aks_name = "${each.value.target}-u-${each.value.regions[0]}-uat-aks"
+    } : {},
+    contains(each.value.envs, "u") && try(each.value.kv_name, "") != "" && length(each.value.regions) > 1 ? {
+      tf_second_uat_aks_apiserver_url         = module.dev_secrets[each.value.name].values["${each.value.target}-u-${each.value.regions[1]}-uat-aks-apiserver-url"].value,
+      tf_second_uat_aks_azure_devops_sa_cacrt = module.dev_secrets[each.value.name].values["${each.value.target}-u-${each.value.regions[1]}-uat-aks-azure-devops-sa-cacrt"].value,
+      tf_second_uat_aks_azure_devops_sa_token = base64decode(module.dev_secrets[each.value.name].values["${each.value.target}-u-${each.value.regions[1]}-uat-aks-azure-devops-sa-token"].value),
+      tf_second_uat_aks_name = "${each.value.target}-u-${each.value.regions[1]}-uat-aks"
+    } : {},
+
+    contains(each.value.envs, "p") && try(each.value.kv_name, "") != "" ? {
+      tf_prod_apiserver_url         = module.dev_secrets[each.value.name].values["${each.value.target}-p-${each.value.regions[0]}-prod-aks-apiserver-url"].value,
+      tf_prod_azure_devops_sa_cacrt = module.dev_secrets[each.value.name].values["${each.value.target}-p-${each.value.regions[0]}-prod-aks-azure-devops-sa-cacrt"].value,
+      tf_prod_azure_devops_sa_token = base64decode(module.dev_secrets[each.value.name].values["${each.value.target}-p-${each.value.regions[0]}-prod-aks-azure-devops-sa-token"].value),
+      tf_prod_aks_name = "${each.value.target}-p-${each.value.regions[0]}-prod-aks"
+    } : {},
+    contains(each.value.envs, "p") && try(each.value.kv_name, "") != "" && length(each.value.regions) > 1 ? {
+      tf_second_prod_aks_apiserver_url         = module.dev_secrets[each.value.name].values["${each.value.target}-p-${each.value.regions[1]}-prod-aks-apiserver-url"].value,
+      tf_second_prod_aks_azure_devops_sa_cacrt = module.dev_secrets[each.value.name].values["${each.value.target}-p-${each.value.regions[1]}-prod-aks-azure-devops-sa-cacrt"].value,
+      tf_second_prod_aks_azure_devops_sa_token = base64decode(module.dev_secrets[each.value.name].values["${each.value.target}-p-${each.value.regions[1]}-prod-aks-azure-devops-sa-token"].value),
+      tf_second_prod_aks_name = "${each.value.target}-p-${each.value.regions[1]}-prod-aks"
+    } : {},
+    {
+      tf_target = each.value.target
+    },
+   local.base_iac_variables_deploy,
+   try(local.target_variables[each.value.target], {}),
+   try(local.domain_variables[each.value.name].iac_variables_deploy, {})
+ )
+
+ variables_secret = merge(
+   local.base_iac_variables_secret,
+   try(local.domain_variables[each.value.name].iac_variables_secrets_deploy, {})
+ )
+
+  service_connection_ids_authorization = [
+    azuredevops_serviceendpoint_github.azure_devops_github_ro.id,
+    module.dev_azurerm_iac_deploy_service_conn.service_endpoint_id,
+    module.uat_azurerm_iac_deploy_service_conn.service_endpoint_id,
+    module.prod_azurerm_iac_deploy_service_conn.service_endpoint_id,
+  ]
+}
--- azure-devops/iac/99_locals.tf
+++ azure-devops/iac/99_locals.tf
@@ -22,7 +22,107 @@ locals {
   srv_endpoint_github_rw = "azure-devops-github-rw"
   srv_endpoint_github_pr = "azure-devops-github-pr"
 
-  domains = [
+  paymon_pipelines = [
+    {
+      name : "elastic-organization-secret",
+      target : "paymon"
+      envs : ["d"],
+      regions = []
+      kv_name : "",
+      rg_name : "",
+      code_review : true,
+      deploy : true,
+      pipeline_prefix : "elastic-organization-secret",
+      pipeline_path : "elastic\\00-organization-secret",
+      repository : {
+        yml_prefix_name = "org-secret"
+        branch_name     = "refs/heads/aks-pipeline"
+      }
+    },
+    {
+      name : "elastic-organization",
+      target : "paymon"
+      envs : ["p"], #only production environment is available for organization
+      regions = []
+      kv_name : "",
+      rg_name : "",
+      code_review : true,
+      deploy : true,
+      pipeline_prefix : "elastic-organization",
+      pipeline_path : "elastic\\01-elastic-organization",
+      repository : {
+        yml_prefix_name = "elastic-org"
+        branch_name     = "refs/heads/aks-pipeline"
+      }
+    },
+    {
+      name : "state-storage",
+      target : "paymon"
+      envs : ["d"],
+      regions = []
+      kv_name : "",
+      rg_name : "",
+      code_review : true,
+      deploy : true,
+      pipeline_prefix : "state-storage",
+      pipeline_path : "elastic\\02-state-storage",
+      repository : {
+        yml_prefix_name = "state-storage"
+        branch_name     = "refs/heads/aks-pipeline"
+      }
+    }
+
+  ]
+
+  pagopa_pipelines = [
+  {
+      name : "pagopa-az-application",
+      target : "pagopa"
+      envs : ["d"],
+      regions = []
+      kv_name : "",
+      rg_name : "",
+      code_review : true,
+      deploy : true,
+      pipeline_prefix : "az-application",
+      pipeline_path : "pagopa\\03-az-application",
+      repository : {
+        yml_prefix_name = "az-app"
+        branch_name     = "refs/heads/aks-pipeline"
+      }
+    },
+    {
+      name : "pagopa-deployment",
+      target : "pagopa"
+      envs : ["d"],
+      regions = []
+      kv_name : "",
+      rg_name : "",
+      code_review : true,
+      deploy : true,
+      pipeline_prefix : "elastic-deployment",
+      pipeline_path : "pagopa\\04-elastic-deployment",
+      repository : {
+        yml_prefix_name = "elastic-deployment"
+        branch_name     = "refs/heads/aks-pipeline"
+      }
+    },
+    {
+      name : "pagopa-secret",
+      target : "pagopa"
+      envs : ["d"],
+      regions = []
+      kv_name : "",
+      rg_name : "",
+      code_review : true,
+      deploy : true,
+      pipeline_prefix : "elastic-secret",
+      pipeline_path : "pagopa\\05-elastic-secret",
+      repository : {
+        yml_prefix_name = "elastic-secret"
+        branch_name     = "refs/heads/aks-pipeline"
+      }
+    },
     {
       name : "pagopa-resources-common",
       target : "pagopa"
@@ -33,7 +133,7 @@ locals {
       code_review : true,
       deploy : true,
       pipeline_prefix : "pagopa-resources-common",
-      pipeline_path : "elastic\\06-resources-common",
+      pipeline_path : "pagopa\\06-resources-common",
       repository : {
         yml_prefix_name = "res-common"
         branch_name     = "refs/heads/aks-pipeline"
@@ -43,19 +143,25 @@ locals {
       name : "pagopa-resources-app",
       target : "pagopa"
       envs : ["d"],
-      regions = ["weu", "itn"]
+      regions = []
       kv_name : "",
       rg_name : "",
       code_review : true,
       deploy : true,
       pipeline_prefix : "pagopa-resources-app",
-      pipeline_path : "elastic\\07-resources-app",
+      pipeline_path : "pagopa\\07-resources-app",
       repository : {
         yml_prefix_name = "res-app"
         branch_name     = "refs/heads/aks-pipeline"
       }
     }
   ]
+
+
+  domains = concat(
+    local.paymon_pipelines, // common components
+    local.pagopa_pipelines  // pagopa components
+  )
 
   domain_variables = {}
 
