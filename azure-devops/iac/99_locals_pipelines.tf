1ee69f99fcf8faebff73deb619adf4e38b2c0ed3
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-02-27 11:16:46 +0100

    refactor-product-pipelines

--- azure-devops/iac/99_locals_pipelines.tf
+++ azure-devops/iac/99_locals_pipelines.tf
@@ -66,105 +66,114 @@ locals {
     }
   ]
 
-  pagopa_core_pipelines = [
+  /*
+    ADD PRODUCTS HERE
+   */
+  products = {
+    pagopa: {
+      deployment: "pagopa",
+      regions: ["weu", "itn"],
+      envs: ["d"]
+    }
+  }
+
+  product_pipelines = flatten([for product_key,product_conf in local.products: [
     {
-      name : "pagopa-az-application",
-      target : "pagopa"
-      envs : ["d"],
+      name : "${product_key}-az-application",
+      target : "${product_key}"
+      envs : product_conf.envs,
       regions = []
       kv_name : "",
       rg_name : "",
       code_review : true,
       deploy : false, # unable to apply because it need PIM permission
-      pipeline_prefix : "az-application",
-      pipeline_path : "pagopa\\core\\03-az-application",
+      pipeline_prefix : "${product_key}-az-application",
+      pipeline_path : "${product_key}\\core\\03-az-application",
       repository : {
         yml_prefix_name = "az-app"
         branch_name     = "refs/heads/main"
       }
     },
     {
-      name : "pagopa-deployment",
-      target : "pagopa"
-      envs : ["d"],
+      name : "${product_key}-deployment",
+      target : "${product_conf.deployment}"
+      envs : product_conf.envs,
       regions = []
       kv_name : "",
       rg_name : "",
       code_review : true,
       deploy : true,
-      pipeline_prefix : "elastic-deployment",
-      pipeline_path : "pagopa\\core\\04-elastic-deployment",
+      pipeline_prefix : "${product_key}-elastic-deployment",
+      pipeline_path : "${product_key}\\core\\04-elastic-deployment",
       repository : {
         yml_prefix_name = "elastic-deployment"
         branch_name     = "refs/heads/main"
       }
     },
     {
-      name : "pagopa-secret",
-      target : "pagopa"
-      envs : ["d"],
+      name : "${product_key}-secret",
+      target : "${product_key}"
+      envs : product_conf.envs,
       regions = []
       kv_name : "",
       rg_name : "",
       code_review : true,
       deploy : true,
-      pipeline_prefix : "elastic-secret",
-      pipeline_path : "pagopa\\core\\05-elastic-secret",
+      pipeline_prefix : "${product_key}-elastic-secret",
+      pipeline_path : "${product_key}\\core\\05-elastic-secret",
       repository : {
         yml_prefix_name = "elastic-secret"
         branch_name     = "refs/heads/main"
       }
     },
     {
-      name : "pagopa-resources-deployment",
-      target : "pagopa"
-      envs : ["d"],
+      name : "${product_key}-resources-deployment",
+      target : "${product_key}"
+      envs : product_conf.envs,
       regions = []
       kv_name : "",
       rg_name : "",
       code_review : true,
       deploy : true,
-      pipeline_prefix : "pagopa-resources-deployment",
-      pipeline_path : "pagopa\\core\\06-resources-deployment",
+      pipeline_prefix : "${product_key}-resources-deployment",
+      pipeline_path : "${product_key}\\core\\06-resources-deployment",
       repository : {
         yml_prefix_name = "elastic-resource-deployment"
         branch_name     = "refs/heads/main"
       }
-    }
-  ]
-
-  pagopa_app_pipelines = [
+    },
     {
-      name : "pagopa-resources-common",
-      target : "pagopa"
-      envs : ["d"],
-      regions = ["weu", "itn"]
-      kv_name : "paymon-%s-pagopa-%s-kv",
-      rg_name : "paymon-%s-pagopa-%s-sec-rg",
+      name : "${product_key}-resources-common",
+      target : "${product_key}"
+      envs : product_conf.envs,
+      regions = product_conf.regions,
+      kv_name : "paymon-%s-${product_key}-%s-kv",
+      rg_name : "paymon-%s-${product_key}-%s-sec-rg",
       code_review : true,
       deploy : true,
-      pipeline_prefix : "pagopa-resources-common",
-      pipeline_path : "pagopa\\app\\06-resources-common",
+      pipeline_prefix : "${product_key}-resources-common",
+      pipeline_path : "${product_key}\\app\\06-resources-common",
       repository : {
         yml_prefix_name = "res-common"
         branch_name     = "refs/heads/main"
       }
     },
     {
-      name : "pagopa-resources-app",
-      target : "pagopa"
-      envs : ["d"],
+      name : "${product_key}-resources-app",
+      target : "${product_key}"
+      envs : product_conf.envs,
       regions = []
       kv_name : "",
       rg_name : "",
       code_review : true,
       deploy : true,
-      pipeline_prefix : "pagopa-resources-app",
-      pipeline_path : "pagopa\\app\\07-resources-app",
+      pipeline_prefix : "${product_key}-resources-app",
+      pipeline_path : "${product_key}\\app\\07-resources-app",
       repository : {
         yml_prefix_name = "res-app"
         branch_name     = "refs/heads/main"
       }
     }
-  ]
+  ]])
+
 }
